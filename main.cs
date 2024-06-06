8c451673f85a56fa2fece4f53e3b400c47695555
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-06 20:57:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Input wind speed");
+    
+   var WindSpeedMPH = Console.ReadLine();   
+    var WindSpeedSplit = WindSpeedMPH.Split(' ');
+      var WindSpeedNumber = WindSpeedSplit[0];
+
+    int WindSpeed;
+int.TryParse(WindSpeedNumber, out WindSpeed);
+  if( WindSpeed >= 157 ){
+    Console.WriteLine( "Category Five Hurricane");
+  }
+    else if( WindSpeed >= 130 && WindSpeed < 157 ){
+     Console.WriteLine("Category Four Hurricane"); 
+    }
+    else if( WindSpeed >= 111 && WindSpeed < 130 ){
+     Console.WriteLine("Category Three Hurricane"); 
+      }
+    else if( WindSpeed >= 96 && WindSpeed < 111 ){
+     Console.WriteLine("Category Two Hurricane"); 
+      }
+    else if( WindSpeed >= 74 && WindSpeed < 96 ){
+     Console.WriteLine("Category One Hurricane"); 
+      }
+    else if(WindSpeed < 74 ){
+     Console.WriteLine("Not a Hurricane"); 
+      }
+    else {
+      Console.WriteLine("Invald Input");
+    }
+  }
+}
